2b91d0f509186befe831d47513064bdf7342451c
Ahmed Elbrmbaly <ahmed.elbrmbaly@gmail.com>
2025-06-09 02:27:32 +0300

    product filter responsiveness enhanced

--- src/app/features/products/product-filters/product-filters.component.ts
+++ src/app/features/products/product-filters/product-filters.component.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-filters',
@@ -19,12 +20,14 @@ import { MatButtonModule } from '@angular/material/button';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class ProductFiltersComponent implements OnInit {
   @Input() currentFilters: ProductFilters = {};
   @Output() filtersChange = new EventEmitter<ProductFilters>();
+  @Output() filtersClosed = new EventEmitter<void>();
 
   filterForm: FormGroup;
 
@@ -80,5 +83,9 @@ export class ProductFiltersComponent implements OnInit {
 
   clearFilters(): void {
     this.filterForm.reset();
+  }
+
+  closeFilters(): void {
+    this.filtersClosed.emit();
   }
 }
