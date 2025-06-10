2d94986f7fef450b2d8a2d086190edee8d5cf322
Hashim Sobhi <hashimsobhi97@gmail.com>
2025-06-10 19:34:45 +0300

    handled filters options

--- src/app/features/products/product-filters/product-filters.component.ts
+++ src/app/features/products/product-filters/product-filters.component.ts
@@ -35,9 +35,9 @@ export class ProductFiltersComponent implements OnInit {
 
   filterForm: FormGroup;
 
-  readonly brands = ['Nike', 'Adidas', 'Puma', 'Reebok', 'New Balance'];
-  readonly sizes = ['SIZE_38', 'SIZE_39', 'SIZE_40', 'SIZE_41', 'SIZE_42', 'SIZE_43', 'SIZE_44', 'SIZE_45'];
-  readonly colors = ['Black', 'White', 'Blue', 'Grey', 'Brown', 'Red'];
+  readonly brands = ['Nike', 'Merrel', 'Adidas', 'Puma', 'Gucci', 'Sketchers', 'Reebok', 'New Balance'];
+  readonly sizes = ['SIZE_35', 'SIZE_36', 'SIZE_37', 'SIZE_38', 'SIZE_39', 'SIZE_40', 'SIZE_41', 'SIZE_42', 'SIZE_43', 'SIZE_44', 'SIZE_45', 'SIZE_46', 'SIZE_47', 'SIZE_48'];
+  readonly colors = ['Black', 'White', 'Blue', 'Grey', 'Brown', 'Red', 'Cream'];
   readonly genders = ['MALE', 'FEMALE'];
   readonly categories = ['SNEAKERS', 'CLASSIC', 'CASUAL'];
   readonly sortOptions = [
