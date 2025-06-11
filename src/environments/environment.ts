c0de0b859ca2e55adc11ef592bc2afbd37512e87
Ibrahim Diab <ibrahim0diab@gmail.com>
2025-06-11 22:16:58 +0300

    Handling the requirements for deployments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8081',
-  apiUrl: 'http://185.69.166.150/',
+  apiUrl: 'http://localhost:8081',
   get imageBaseUrl() {
     return this.apiUrl;
   }
