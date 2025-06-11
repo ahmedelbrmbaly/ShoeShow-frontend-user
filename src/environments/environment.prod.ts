f90cedfcadd467ac281767dfb72abfb5822490da
Ibrahim Diab <ibrahim0diab@gmail.com>
2025-06-11 17:07:17 +0300

    Handling the requirements for deployments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://185.69.166.150:8081',  // Replace this with your actual production API domain
+  apiUrl: 'http://185.69.166.150:8081',  // Replace this with your actual production API domain
   get imageBaseUrl() {
     return this.apiUrl;
   }
